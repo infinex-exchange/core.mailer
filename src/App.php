4398e3b8b6237f5a2aec6bb460e2f78684ebb69e
infinex-exchange <admin@infinex.cc>
2023-09-08 01:19:17 +0200

    Add mailer.php and App.php

---
+++ src/App.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__.'/Sender.php';
+require __DIR__.'/MailQueue.php';
+require __DIR__.'/MailStorage.php';
+
+class App extends Infinex\App\Daemon {
+    private $pdo;
+    private $sender;
+    private $queue;
+    private $storage;
+    
+    function __construct() {
+        parent::__construct('auth.api-auth');
+        
+        $this -> pdo = new Infinex\Database\PDO($this -> loop, $this -> log);
+        $this -> pdo -> start();
+        
+        $this -> sender = new Sender($this -> log);
+        
+        $this -> queue = new MailQueue($this -> log, $this -> sender);
+        
+        $this -> storage = new MailStorage($this -> loop, $this -> log, $this -> sender);
+        $this -> storage -> start();
+        
+        $th = $this;
+        $this -> amqp -> on('connect', function() use($th) {
+            $th -> queue -> bind($th -> amqp);
+        });
+    }
+}
+
+?>
