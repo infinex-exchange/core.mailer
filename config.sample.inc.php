f0d01cb3ae31dcb0965fe22b413e9a6793a4399f
infinex-exchange <admin@infinex.cc>
2023-09-08 01:13:40 +0200

    V2

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -1,17 +1,23 @@
 <?php
 
-define('DEBUG_MODE', true);
+define('LOG_LEVEL', 0);
+
+define('RMQ_HOST', 'localhost');
+define('RMQ_PORT', 5672);
+define('RMQ_USER', 'mailer');
+define('RMQ_PASS', 'password');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'mailer');
 define('DB_PASS', 'password');
-define('DB_NAME', 'vayamos');
+define('DB_NAME', 'mailer');
 
-define('MAIL_HOST', 'localhost');
-define('MAIL_PORT', 587);
-define('MAIL_AUTH', true);
-define('MAIL_USER', 'app@infinex.cc');
-define('MAIL_PASS', 'password');
+define('SMTP_HOST', 'localhost');
+define('SMTP_PORT', 587);
+define('SMTP_AUTH', true);
+define('SMTP_USER', 'app@infinex.cc');
+define('SMTP_PASS', 'password');
+
 define('MAIL_FROM', 'noreply@infinex.cc');
 define('MAIL_FROM_NAME', 'Infinex');
 
---
+++ src/Sender.php
@@ -0,0 +1,69 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception as PHPMailerException;
+
+class Sender {
+    private $log;
+    
+    function __construct($log) {
+        $this -> log = $log;
+        
+        $this -> log -> debug('Initialized sender');
+    }
+    
+    public function mail($to, $template, $data) {
+        $phpMailer = new PHPMailer(true);
+
+        // SMTP server settings
+        $phpMailer -> IsSMTP();
+        $phpMailer -> Host = SMTP_HOST;
+        $phpMailer -> Port = SMTP_PORT;
+        $phpMailer -> SMTPAutoTLS = true;
+        $phpMailer -> SMTPAuth = SMTP_AUTH;
+        if(SMTP_AUTH) {
+            $phpMailer -> Username = SMTP_USER;
+            $phpMailer -> Password = SMTP_PASS;
+        }
+            
+        // Message general
+        $phpMailer -> CharSet = 'UTF-8';
+        $phpMailer -> setFrom(MAIL_FROM, MAIL_FROM_NAME);
+        $phpMailer -> isHTML(true);
+        $phpMailer -> addEmbeddedImage(__DIR__.'/mail-templates/logo.png', 'logo');
+            
+        // Recipient address
+        $phpMailer -> addAddress($to);
+
+        // Template
+        $tpl = file_get_contents(__DIR__.'/mail-templates/header.html')
+             . file_get_contents(__DIR__.'/mail-templates/templates/'.$template.'.html')
+             . file_get_contents(__DIR__.'/mail-templates/footer.html');
+            
+        // Data
+        $data['email'] = $to;
+        $data['email_urlencoded'] = urlencode($to);
+        foreach($data as $k => $v) {
+            $tpl = str_replace('{{' . $k . '}}', $v, $tpl);
+        }
+            
+        // Extract subject from template
+        $phpMailer -> Subject = '';
+        $subjectTag = strpos($tpl, '[[SUBJECT: ');
+        if($subjectTag !== false) {
+            $subjectEndTag = strpos($tpl, ']]', $subjectTag);
+            if($subjectEndTag !== false) {
+                $subject = substr($tpl, $subjectTag + 11, $subjectEndTag - $subjectTag - 11);
+                $tpl = str_replace("[[SUBJECT: $subject]]", '', $tpl);
+                $phpMailer -> Subject = $subject;
+            }
+        }
+        $phpMailer -> Body = $tpl;  
+
+        // Send
+        $phpMailer -> send();
+    }
+}
+
+?>
